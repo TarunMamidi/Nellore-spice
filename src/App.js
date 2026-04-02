dcfdc5863f6d99b230275c45654b76ad505302d2
TarunMamidi <tarunmamidi8@gmail.com>
2026-04-02 09:45:03 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+import React, { useState } from 'react';
 import './App.css';
+import Menu from './components/menu/Menu';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+
 
 function App() {
+
+const [navTheme,setNavTheme] = useState("light");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar theme={navTheme}/>
+      <Hero setNavTheme={setNavTheme}/>
+      <Menu/>
+      <About/>
+      <Contact/>
+     
+      
     </div>
   );
 }
